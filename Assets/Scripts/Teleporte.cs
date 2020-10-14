531a39ef4840abb0e7e75b1933950f4b2851a108
Lucas Lima <lucas_felippe@outlook.com.br>
2020-10-13 23:26:21 -0300

    Criação da funcionalidade de teletransporte

---
+++ Assets/Scripts/Teleporte.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(AudioSource))]
+public class Teleporte : MonoBehaviour
+{
+    public Transform destino;
+    public AudioClip audioTeleporte;
+    public bool destruirAoColidir = false;
+    AudioSource emissorDeSom;
+    bool rotinaIniciada = false;
+
+    void Awake(){
+        emissorDeSom = GetComponent<AudioSource> ();
+        emissorDeSom.playOnAwake = false;
+        emissorDeSom.loop = false;
+        GetComponent<BoxCollider> ().isTrigger = true;  
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(destino && !rotinaIniciada){
+            other.transform.position = destino.position;
+            other.transform.rotation = destino.rotation;
+        }
+
+        if(audioTeleporte){
+            emissorDeSom.clip = audioTeleporte;
+            emissorDeSom.PlayOneShot (emissorDeSom.clip);
+        }
+    }
+}
